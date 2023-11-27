a1aa486b529f9b5b51d0b2d69708815677b8acb4
kristianche <chelebiev777@gmail.com>
2023-11-27 18:02:52 +0200

    Added the last 3 tasks for the lection

---
+++ 04. Conditional Statements Exercise/06. World Swimming Record/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+var record = double.Parse(Console.ReadLine());
+var distance = double.Parse(Console.ReadLine());
+var secondsPer1Meter = double.Parse(Console.ReadLine());
+
+var additionalTime = Math.Floor(distance / 15) * 12.5;
+var totalTime = (distance * secondsPer1Meter) + additionalTime;
+
+if (totalTime < record)
+{
+    Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:F2} seconds.");
+} else
+{
+    Console.WriteLine($"No, he failed! He was {(totalTime - record):F2} seconds slower.");
+}
---
+++ 04. Conditional Statements Exercise/08. Lunch Break/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+double timeForLunch = 8;
+double timeForRest = 4;
+
+string name = Console.ReadLine();
+int durationEpisode = int.Parse(Console.ReadLine());
+int durationBreak = int.Parse(Console.ReadLine());
+
+double timeLunch = durationBreak / timeForLunch;
+double timeRest = durationBreak / timeForRest;
+double timeLeft = durationBreak - timeLunch - timeRest;
+
+double leftTime = Math.Abs(durationEpisode - timeLeft);
+
+if (timeLeft >= durationEpisode)
+{
+    Console.WriteLine($"You have enough time to watch {name} and left with {Math.Ceiling(leftTime)} minutes free time.");
+}
+else
+{
+    Console.WriteLine($"You don't have enough time to watch {name}, you need {Math.Ceiling(leftTime)} more minutes.");
+}
