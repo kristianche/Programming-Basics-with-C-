c824a44b7813c59012dff6bc239abacb4d8e9cb9
kristianche <chelebiev777@gmail.com>
2023-11-27 18:35:01 +0200

    Added the the first fourth exercises of the lection: Conditional Statements Advanced

---
+++ 05. Conditional Statements Advanced/02. Weekend or Working Day/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+var day = Console.ReadLine();
+
+switch (day)
+{
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday":
+        Console.WriteLine("Working day");
+        break;
+
+    case "Saturday":
+    case "Sunday":
+        Console.WriteLine("Weekend");
+        break;
+
+    default:
+        Console.WriteLine("Error");
+        break;
+}
---
+++ 05. Conditional Statements Advanced/03. Animal Type/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+var animal = Console.ReadLine();
+
+switch (animal)
+{
+    case "dog":
+        Console.WriteLine("mammal");
+        break;
+
+    case "crocodile":
+    case "tortoise":
+    case "snake":
+        Console.WriteLine("reptile");
+        break;
+
+    default:
+        Console.WriteLine("unknown");
+        break;
+}
